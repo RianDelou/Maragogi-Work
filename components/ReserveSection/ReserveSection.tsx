e3d89d3defec7d220604a4c0552b45d094c6697c
Rian Delou <riandelou63@hotmail.com>
2024-11-21 17:43:18 -0300

    finish

--- components/ReserveSection/ReserveSection.tsx
+++ components/ReserveSection/ReserveSection.tsx
@@ -1,16 +1,16 @@
-'use client';
+"use client";
 import PriceImage from "./PriceImage";
 import ReservationCalendar from "./ReservationCalendar";
 
 export default function ReserveSection() {
   return (
     <section className="relative flex flex-col items-center justify-start min-h-[100vh] w-full bg-[#fff8e8]">
-      {/* Título */}
+    
       <h1 className="text-center text-[#f85600] text-4xl font-bold my-4">
         FÉRIAS DE JULHO NO SALINAS MARAGOGI É <br /> DIVERSÃO GARANTIDA
       </h1>
 
-      {/* Descrição */}
+      
       <p className="text-center text-[#847758] text-lg max-w-4xl mx-auto my-4">
         Férias de Julho é sinônimo de diversão em família! No Salinas Maragogi,
         as crianças são superestrelas com estadia grátis*, Clubinho Infantil
@@ -18,25 +18,22 @@ export default function ReserveSection() {
         paladares e muito mais!
       </p>
 
-      {/* Imagem de Preço */}
       <PriceImage />
-
-      {/* Fundo decorativo */}
       <div
-        className="absolute w-full h-[250px] mt-96"
-        style={{
-          backgroundImage: 'url("/conchas.png")',
-          backgroundSize: "contain",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "center",
-        }}
-      ></div>
+  className="absolute w-full h-[250px] mt-96 hidden md:block"
+  style={{
+    backgroundImage: 'url("/conchas.png")',
+    backgroundSize: "contain",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "center",
+  }}
+></div>
 
-      {/* Calendário */}
+
+     
       <div className="relative z-10 mt-8">
         <ReservationCalendar />
       </div>
     </section>
   );
 }
-
